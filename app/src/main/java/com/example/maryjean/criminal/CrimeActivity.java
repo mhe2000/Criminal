e635efe02ef832b250ff0b45434768fb77a7b41b
Maryjean <mhe2000@wildcats.unh.edu>
2016-03-03 21:51:06 -0500

    trying to commit again

--- app/src/main/java/com/example/maryjean/criminal/CrimeActivity.java
+++ app/src/main/java/com/example/maryjean/criminal/CrimeActivity.java
@@ -1,6 +1,7 @@
 package com.example.maryjean.criminal;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,5 +12,15 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
