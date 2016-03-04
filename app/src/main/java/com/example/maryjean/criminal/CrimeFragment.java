dc0cd4f291763c958b68cf00c7f199406120cba0
Maryjean <mhe2000@wildcats.unh.edu>
2016-03-03 19:39:05 -0500

    Initial Commit

--- app/src/main/java/com/example/maryjean/criminal/CrimeFragment.java
+++ app/src/main/java/com/example/maryjean/criminal/CrimeFragment.java
@@ -27,25 +27,6 @@ public class CrimeFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
 
-        mTitleField = (EditText)v.findViewById(R.id.crime_title);
-        mTitleField.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(
-                    CharSequence s, int start, int before, int count){
-                mCrime.setTitle(s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
         return v;
     }
 }
